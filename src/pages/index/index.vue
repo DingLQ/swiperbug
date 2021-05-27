0f2155303ae7ad32ef32328c937399540ac2fe67
丁龙强 <dlqym@qq.com>
2021-05-27 14:47:34 +0800

    bug 提交

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,46 +1,36 @@
 <template>
   <view class="index">
-    <view class="index">
-    <AtNoticebar marquee>
-      欢迎使用 Taro UI Vue
-    </AtNoticebar>
-    <AtButton
-      type="primary"
-      :on-click="handleClick"
-    >
-      AtButton
-    </AtButton>
-    <AtToast :is-opened="show" :text="msg" :on-close="handleClose"></AtToast>
-  </view>
+    <swiper
+      indicatorColor='#999'
+      indicatorActiveColor='#333'
+      current="current"
+      :duration="700"
+      :circular="true"
+      :autoplay="true"
+      :indicatorDots="show">
+      <swiper-item v-for="(item, idx) in imgUrls" :key="idx">
+        <image :src="item" class="slide-image" />
+      </swiper-item>
+    </swiper>
   </view>
 </template>
 
 <script>
 // 按需引入, 更小的应用体积
-import { AtButton, AtToast, AtNoticebar } from 'taro-ui-vue'
-import "taro-ui-vue/dist/style/components/button.scss"
-import "taro-ui-vue/dist/style/components/toast.scss"
-import "taro-ui-vue/dist/style/components/noticebar.scss"
-import './index.scss' 
+import './index.scss'
 export default {
-    components: {
-    AtButton,
-    AtToast,
-    AtNoticebar
-  },
+    components: {},
   data () {
     return {
       msg: 'Hello world!',
-      show: false
+      imgUrls: [
+        'https://img10.360buyimg.com/babel/s700x360_jfs/t25855/203/725883724/96703/5a598a0f/5b7a22e1Nfd6ba344.jpg!q90!cc_350x180',
+        'https://img11.360buyimg.com/babel/s700x360_jfs/t1/4776/39/2280/143162/5b9642a5E83bcda10/d93064343eb12276.jpg!q90!cc_350x180',
+        'https://img14.360buyimg.com/babel/s700x360_jfs/t1/4099/12/2578/101668/5b971b4bE65ae279d/89dd1764797acfd9.jpg!q90!cc_350x180',
+      ],
+      show: true,
     }
   },
-  methods: {
-    handleClick () {
-      this.show = true
-    },
-    handleClose () {
-      this.show = false
-    }
-  },
+  methods: {},
     }
 </script>
